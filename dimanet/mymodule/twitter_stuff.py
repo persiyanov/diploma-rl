58dbdf05caf1000cc6d1eac31c3256f5d8d0a0cd
persiyanov <persiyanov@phystech.edu>
2017-05-15 23:19:39 +0300

    iterator for dssm

--- dimanet/mymodule/twitter_stuff.py
+++ dimanet/mymodule/twitter_stuff.py
@@ -3,6 +3,9 @@ from __future__ import unicode_literals
 
 import json
 import codecs
+import numpy as np
+
+from collections import defaultdict
 
 from background_generator import background
 from base_stuff import phrase2matrix
@@ -50,3 +53,55 @@ def iterate_minibatches_twitter(filename, vocab, batch_size, context_size=2):
 
                 batch_context = next_context
                 batch_answer = next_answer
+
+
+def _sample_from_but(arr, exclude=()):
+    res = np.random.choice(arr)
+    while res in exclude:
+        res = np.random.choice(arr)
+    return res
+
+
+def _sample_utt(arr, delete=False):
+    idx = np.random.choice(np.arange(len(arr)))
+    res = arr[idx]
+    if delete:
+        arr.remove(idx)
+    return res
+
+
+@background
+def iterate_minibatches_twitter_dssm(filename, vocab, batch_size):
+    """FIle at 'filename' has the following format:
+    534\tахаххаха приветосикиииа
+    """
+    uid2msgs = defaultdict(list)
+    with codecs.open(filename, encoding='utf8') as fin:
+        for line in fin:
+            chunks = line.strip().split('\t')
+            id_, msg = chunks[0], ' '.join(chunks[1:])
+            uid2msgs[id_].append(msg)
+    uid2msgs_copy = uid2msgs.copy()
+
+    while len(uid2msgs) > 0:
+        batch_uid = []
+        batch_good_utt = []
+        batch_bad_utt = []
+        for i in xrange(batch_size):
+            uid1 = _sample_from_but(uid2msgs.keys())
+            uid2 = _sample_from_but(uid2msgs.keys(), exclude=[uid1])
+
+            good_utt = _sample_utt(uid2msgs[uid1], delete=True)
+            if len(uid2msgs[uid1]) == 0:
+                uid2msgs.pop(uid1)
+            bad_utt = _sample_utt(uid2msgs_copy[uid2])
+
+            batch_uid.append(uid1), batch_good_utt.append(good_utt), batch_bad_utt.append(bad_utt)
+
+            if len(uid2msgs) == 0:
+                break
+
+        yield (np.array(batch_uid),
+               phrase2matrix(batch_good_utt, vocab, normalize=True),
+               phrase2matrix(batch_bad_utt, vocab, normalize=True)
+               )
